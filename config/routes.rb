b88c558752693dee00220f9d655bea63b69847f0
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-19 11:37:16 -0600

    Add method for getting and posting on tasks page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
 end
